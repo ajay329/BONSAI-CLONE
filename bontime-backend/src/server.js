b46839abdb89da91db777707bd2dc0ded6839253
furqan5921 <101380683+furqan5921@users.noreply.github.com>
2022-11-09 18:07:34 +0530

    basic setup of backend done

--- bontime-backend/src/server.js
+++ bontime-backend/src/server.js
@@ -1,6 +1,15 @@
 require("dotenv").config()
 const express = require("express");
+const PORT =process.env.PORT;
+const connect = require("./config/db")
 const cors = require("cors")
 
 const app = express()
 
+app.use(cors())
+app.use(express.json())
+
+app.listen(PORT,async()=>{
+    await connect();
+    console.log(`Server is running on port http://localhost:${PORT}`);
+})
