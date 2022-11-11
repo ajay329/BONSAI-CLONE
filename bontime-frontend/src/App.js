31fe6d5daf36f7af0c6c45554e2f574c2844646c
mdalimujtaba <mdalimujtaba29850@gmil.com>
2022-11-11 10:46:40 +0530

    edited

--- bontime-frontend/src/App.js
+++ bontime-frontend/src/App.js
@@ -1,15 +1,12 @@
 
 import './App.css';
 import AllRoutes from './AllRoutes/AllRoutes';
-import { BonsaiCash } from './Pages/ProductPage/Bonsai_Cash';
-import { BonsaiTax } from './Pages/ProductPage/Bonsai_Tax';
+
 
 function App() {
   return (
     <div className="App">
       <AllRoutes/>
-      {/* <BonsaiCash/> */}
-      {/* <BonsaiTax/> */}
     </div>
   );
 }
