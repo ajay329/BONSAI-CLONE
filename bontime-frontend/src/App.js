6d2094ed93177bdcecfbd831bd35775f7b718cb1
mdalimujtaba <mdalimujtaba29850@gmil.com>
2022-11-11 21:15:38 +0530

    removed

--- bontime-frontend/src/App.js
+++ bontime-frontend/src/App.js
@@ -1,16 +1,14 @@
 import './App.css';
 import AllRoutes from './AllRoutes/AllRoutes';
-import { BonsaiCash } from './Pages/ProductPage/Bonsai_Cash';
-import { BonsaiTax } from './Pages/ProductPage/Tax_page';
+
 
 
 
 function App() {
   return (
     <div className="App">
-      {/* <AllRoutes/> */}
-      {/* <BonsaiCash/> */}
-      <BonsaiTax/>
+      <AllRoutes/>
+      
     </div>
   );
 }
