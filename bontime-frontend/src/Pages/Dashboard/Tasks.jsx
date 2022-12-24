59f7e0c6ff0d2212550bfde36b56d7fa5d82339f
rakeshrakeshyadav <rakeshy84@yahoo.com>
2022-12-24 20:32:46 +0530

    day-3

--- bontime-frontend/src/Pages/Dashboard/Tasks.jsx
+++ bontime-frontend/src/Pages/Dashboard/Tasks.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import React,{ useState } from 'react';
+import TodoForm from "../../components/Tasks/TodoForm";
+import TodoList from "../../components/Tasks/TodoList";
+
+import "./Tasks.css"
 
 const Tasks = () => {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [editId, setEditId] = useState(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (editId) {
+      const editTodo = todos.find((i) => i.id === editId);
+      const updatedTodos = todos.map((t) =>
+        t.id === editTodo.id
+          ? (t = { id: t.id, todo })
+          : { id: t.id, todo: t.todo }
+      );
+      setTodos(updatedTodos);
+      setEditId(0);
+      setTodo("");
+      return;
+    }
+
+    if (todo !== "") {
+      setTodos([{ id: `${todo}-${Date.now()}`, todo }, ...todos]);
+      setTodo("");
+    }
+  };
+
+  const handleDelete = (id) => {
+    const delTodo = todos.filter((to) => to.id !== id);
+    setTodos([...delTodo]);
+  };
+
+  const handleEdit = (id) => {
+    const editTodo = todos.find((i) => i.id === id);
+    setTodo(editTodo.todo);
+    setEditId(id);
+  };
+
   return (
-    <div>Tasks</div>
-  )
+    <div >
+      <div className="container">
+        <h1>TASK</h1>
+        <TodoForm
+          handleSubmit={handleSubmit}
+          todo={todo}
+          editId={editId}
+          setTodo={setTodo}
+        />
+
+        <TodoList
+          todos={todos}
+          handleEdit={handleEdit}
+          handleDelete={handleDelete}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Tasks
